0722bea0d88e6f77926d5ef9b4fe120d3127f379
Darshan <iamdarshan6239db@gmail.com>
2023-02-24 10:34:09 +0530

    Commit

--- app/src/main/java/com/darshan/radio_group/MainActivity.java
+++ app/src/main/java/com/darshan/radio_group/MainActivity.java
@@ -1,6 +1,7 @@
 package com.darshan.radio_group;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,27 +11,31 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    RadioGroup RG;
+    RadioGroup rgp;
     Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Binding();
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                int id = RG.getCheckedRadioButtonId();
-                RadioButton RB = findViewById(id);
 
-                Toast.makeText(MainActivity.this,"Your Gender is="+RB.getText().toString(),Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                int id = rgp.getCheckedRadioButtonId();
+                RadioButton rb =findViewById(id);
+                Toast.makeText(MainActivity.this,"Your Gender is ="+rb.getText().toString(),Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private void Binding() {
-        RG.findViewById(R.id.RG);
-        btn.findViewById(R.id.btn);
+        rgp = findViewById(R.id.rg);
+        btn = findViewById(R.id.btn);
     }
+
+
 }
